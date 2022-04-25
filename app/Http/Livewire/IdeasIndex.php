223fd7fab69b7de6788ed3975539d778516d5f59
Sublime12 <tshimangasublime@gmail.com>
2022-04-25 19:39:48 +0100

    Episode 24 - Status Filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -4,18 +4,25 @@ namespace App\Http\Livewire;
 
 use App\Models\Idea;
 use App\Models\Vote;
+use Illuminate\Http\Request;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use Livewire\WithPagination;
+use App\Models\Status;
 
 class IdeasIndex extends Component
 {
-    use WithPagination;
+    // use WithPagination;
 
     public function render()
     {
+        $statuses = Status::all()->pluck('id', 'name');
+
         return view('livewire.ideas-index', [
             'ideas' => Idea::with('user', 'category', 'status')
+                ->when(request()->status && request()->status !== 'All', function ($query) use ($statuses) {
+                    return $query->where('status_id', $statuses->get(request()->status));
+                })
                 ->addSelect(['voted_by_user' => Vote::select('id')
                     ->where('user_id', Auth::id())
                     ->whereColumn('idea_id', 'ideas.id')
